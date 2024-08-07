be530125044e4d8945f138fc81701d64905b386d
Maaz Ahmad <maazahamd2404@gmail.com>
2024-08-08 00:06:57 +0500

    removed buggy images and created combined dataset

--- feature_eng.py
+++ feature_eng.py
@@ -1,3 +1,32 @@
 import tensorflow as tf
 import os
+import cv2
+import imghdr
+import matplotlib.pyplot as plt
 
+data_dir = 'data'
+image_exts = ['jpeg','jpg','bmp','png']
+
+#looping through and checking and removing if theres a buggy image
+for image_class in os.listdir(data_dir): 
+    for image in os.listdir(os.path.join(data_dir, image_class)):
+        image_path = os.path.join(data_dir, image_class, image)
+        try: 
+            img = cv2.imread(image_path)
+            tip = imghdr.what(image_path)
+            if tip not in image_exts: 
+                print('Image not in ext list {}'.format(image_path))
+                os.remove(image_path)
+        except Exception as e: 
+            print('Issue with image {}'.format(image_path))
+            # os.remove(image_path)
+
+import numpy as np
+data = tf.keras.utils.image_dataset_from_directory('data')
+data_iterator = data.as_numpy_iterator()
+batch = data_iterator.next()
+fig, ax = plt.subplots(ncols=4, figsize=(20,20))
+for idx, img in enumerate(batch[0][:4]):
+    ax[idx].imshow(img.astype(int))
+    ax[idx].title.set_text(batch[1][idx]) 
+  # 2- united 0 - arsenal 1- liverpool                        
